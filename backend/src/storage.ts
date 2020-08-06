790d9f82a2b511e9f96971af5464823842bbb984
Daven DU <davendu@tencent.com>
2020-08-06 23:22:42 +0800

    feat: add anti-fool for team/service configs

--- backend/src/storage.ts
+++ backend/src/storage.ts
@@ -13,25 +13,41 @@ let config: Config;
 
 async function reload(filename: string): Promise<Config> {
     const file = await read(filename, { encoding: "utf-8" })
-    let m: Config = await YAML.parse(file)
-    for (let i = 0; i < m.service.length; i++) {
-        if (!m.service[i].stealthPort) {
-            m.service[i].stealthPort = m.service[i].normalPort + 10000;
+    let newConfig: Config = await YAML.parse(file)
+    for (let i = 0; i < newConfig.service.length; i++) {
+        if (!newConfig.service[i].stealthPort) {
+            newConfig.service[i].stealthPort = newConfig.service[i].normalPort + 10000;
         }
     }
-    m.updatedAt = Date.now()
-    return m
+    newConfig.updatedAt = Date.now()
+    // Logic check
+    // There should be no teams/services with the same name
+    let teams: string[] = []
+    let services: string[] = []
+    newConfig.team.forEach(m => teams.push(m.name.trim()))
+    newConfig.service.forEach(m => services.push(m.name.trim()))
+    if (teams.length != new Set(teams).size) {
+        throw "Duplicate team name!"
+    }
+    if (services.length != new Set(services).size) {
+        throw "Duplicate service name!"
+    }
+    return newConfig
 }
 
 export function getConfig() { return config }
 
 
 export async function startWatch(filename: string, pubsub: PubSub) {
-    config = await reload(filename)
     // One-liner for current directory
     chokidar.watch(filename).on('all', async (event, path) => {
         console.log(`Received ${event} for ${path}`)
-        config = await reload(filename);
+        try {
+            config = await reload(filename);
+        } catch (e){
+            console.log(`Reload failed: ${e}`)
+            return
+        }
         // if config changed, push a full-reload notification
         pubsub.publish(SubscriptionType.Config, {config: config})
     });
